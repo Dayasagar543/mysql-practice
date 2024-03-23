8807948c7018011dd33448d6920aefc352b89ef9
Dayasagar543 <dayasagar.543@gmail.com>
2024-03-23 23:05:42 +0530

    written the file for creating and dropping the databases.

---
+++ practice folder1/intro _creation_deletion.sql
@@ -0,0 +1,22 @@
+-- for logging into the database or the mysql server account from command prompt you should use the following command.
+-- command:
+    --  mysql -u (root/account user name) -p then enter
+    --  and it will ask for password enter the account password
+
+
+
+-- creating databases
+
+create database nexus;
+
+-- for seeing the availabe databases
+
+show databases;
+
+-- working and using the database is like as follows
+
+use nexus;
+
+--for deleting the database
+
+drop DATABASE nexus;
